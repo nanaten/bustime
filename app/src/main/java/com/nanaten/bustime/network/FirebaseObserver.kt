9efad925f497ce59bc9d1f296d1f47e78eddbe1d
m.coder <kurokoshow00@gmail.com>
2020-01-31 17:20:23 +0900

    add FirebaseObserver

---
+++ app/src/main/java/com/nanaten/bustime/network/FirebaseObserver.kt
@@ -0,0 +1,51 @@
+/*
+ * Created by m.coder on 2020/1/31.
+ * Copyright (c) 2020. nanaten All rights reserved.
+ */
+
+package com.nanaten.bustime.network
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.nanaten.bustime.entity.CalendarEntity
+import com.nanaten.bustime.entity.Diagram
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.flow
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class FirebaseObserver {
+    private val firestore = FirebaseFirestore.getInstance()
+
+    companion object {
+        const val calendar = "calendar"
+        const val date = "date"
+        const val second = "second"
+    }
+
+    suspend fun getTodayCalendar(): Flow<CalendarEntity> {
+        val today = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
+        @UseExperimental(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+        return channelFlow {
+            val listenerRegistration = firestore.collection(calendar).whereEqualTo(date, today)
+                .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    if (firebaseFirestoreException != null) {
+                        channel.close(firebaseFirestoreException)
+                    } else {
+                        val calendar =
+                            querySnapshot?.documents?.map { it.toObject(CalendarEntity::class.java) }
+                        channel.offer(calendar?.firstOrNull() ?: CalendarEntity())
+                    }
+                }
+            awaitClose {
+                listenerRegistration.remove()
+            }
+        }
+    }
+
+    suspend fun getDiagrams(): Flow<List<Diagram>> {
+        return flow { listOf<Diagram>() }
+    }
+}
---
+++ app/src/main/java/com/nanaten/bustime/ui/DiagramViewModel.kt
@@ -0,0 +1,12 @@
+/*
+ * Created by m.coder on 2020/1/31.
+ * Copyright (c) 2020. nanaten All rights reserved.
+ */
+
+package com.nanaten.bustime.ui
+
+import androidx.lifecycle.ViewModel
+
+class DiagramViewModel : ViewModel() {
+    
+}
