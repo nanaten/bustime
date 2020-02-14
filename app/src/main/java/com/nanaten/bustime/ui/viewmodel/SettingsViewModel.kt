0dfbe1ce947d4953d26d7f19578023b190707cc3
m.coder <kurokoshow00@gmail.com>
2020-02-14 11:48:54 +0900

    fix SettingsViewModel

--- app/src/main/java/com/nanaten/bustime/ui/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/com/nanaten/bustime/ui/viewmodel/SettingsViewModel.kt
@@ -5,11 +5,39 @@
 
 package com.nanaten.bustime.ui.viewmodel
 
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.nanaten.bustime.BuildConfig
+import com.nanaten.bustime.SharedPref
 
 class SettingsViewModel : ViewModel() {
+    val isDarkMode = MutableLiveData<Boolean>()
+    val firstView = MutableLiveData<Int>()
+
     fun getVersion(): String {
         return BuildConfig.VERSION_NAME
+    }
+
+    fun getIsDarkMode(context: Context): Boolean {
+        val isDarkMode = SharedPref(context).getIsDarkMode()
+        this.isDarkMode.postValue(isDarkMode)
+        return isDarkMode
+    }
+
+    fun setIsDarkMode(context: Context) {
+        val value = isDarkMode.value ?: false
+        SharedPref(context).setIsDarkMode(value)
+    }
+
+    fun getFirstView(context: Context): Int {
+        val firstView = SharedPref(context).getFirstViewSetting()
+        this.firstView.postValue(firstView)
+        return firstView
+    }
+
+    fun setFirstView(context: Context) {
+        val value = firstView.value ?: 0
+        SharedPref(context).setFirstViewSetting(value)
     }
 }
