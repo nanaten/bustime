f061ee0b55c113ea5c8ef64c73c60f6e3a216876
m.coder <kurokoshow00@gmail.com>
2020-05-07 08:29:00 +0900

    add: Room用のDatabaseを追加

---
+++ app/src/main/java/com/nanaten/bustime/network/room/DiagramDatabase.kt
@@ -0,0 +1,16 @@
+/*
+ * Created by m.coder on 2020/5/7.
+ * Copyright (c) 2020. nanaten All rights reserved.
+ */
+
+package com.nanaten.bustime.network.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.nanaten.bustime.network.entity.CalendarEntity
+import com.nanaten.bustime.network.entity.DiagramEntity
+
+@Database(entities = arrayOf(CalendarEntity::class, DiagramEntity::class), version = 1)
+abstract class DiagramDatabase : RoomDatabase() {
+    abstract fun calendarDao(): CalendarDao
+}
